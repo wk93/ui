4e7b24e2f4308d33c2f7825b67f3698ee1b049bf
Wojtek <wojtek@kania.sh>
2021-07-13 12:36:42 +0200

    Initial commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,9 @@
+import 'tailwindcss/tailwind.css';
+import './app.css';
+import { AppProps } from 'next/app';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import { NextPage } from 'next';
+import Head from 'next/head';
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Test</title>
+      </Head>
+
+      <main>
+        <h1>Test</h1>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
